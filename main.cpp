962e22a690d49e59369c4d5bbaa7703f078f0c77
jsalvatto <jsalvatto@outlook.com>
2024-09-28 13:07:40 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// COMSC-210 | Lab 11 | Joseph Salvatto
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <fstream>
+using namespace std;
+
+int main() {
+    cout<<"Hello World"<<endl;
+    
+
+return 0;
+}
